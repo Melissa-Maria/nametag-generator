30063bf61ca071f6a9848ca05e643e542492e0c1
Melissa Pignalosa <m.pignalosa@hotmail.com>
2024-03-07 17:33:05 +0800

    Created a stateful component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import NameTag from "./NameTag.js";
+//import names array
 
 import "./styles.css";
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-  </div>
-);
+const renderNameTag = (name) => <NameTag key={name} name={name} />;
+
+class App extends Component {
+  state = { names: ["Mel", "Jake", "Frankie", "Allirra", "Michelle"] };
+
+  render() {
+    const NameTagElements = this.state.names.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
